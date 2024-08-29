1d07b52fe580f303a620ba467497c388799f65ca
Davood Wadi <62418145+davoodwadi@users.noreply.github.com>
2024-08-30 01:07:12 +0330

    commented out twitter

--- routes/auth.js
+++ routes/auth.js
@@ -6,23 +6,23 @@ var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 var db = require("../db");
 
-passport.use(
-  new TwitterStrategy(
-    {
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: "/oauth/callback/twitter",
-    },
-    function (token, tokenSecret, profile, cb) {
-      console.log("profile", profile);
-      var user = {
-        id: 1,
-        name: profile.displayName,
-      };
-      return cb(null, user);
-    }
-  )
-);
+// passport.use(
+//   new TwitterStrategy(
+//     {
+//       consumerKey: process.env.TWITTER_CONSUMER_KEY,
+//       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+//       callbackURL: "/oauth/callback/twitter",
+//     },
+//     function (token, tokenSecret, profile, cb) {
+//       console.log("profile", profile);
+//       var user = {
+//         id: 1,
+//         name: profile.displayName,
+//       };
+//       return cb(null, user);
+//     }
+//   )
+// );
 
 console.log(process.env.GOOGLE_CLIENT_ID);
 passport.use(
@@ -39,55 +39,6 @@ passport.use(
         name: profile.displayName,
       };
       return cb(null, user);
-      // db.get(
-      //   "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
-      //   ["https://www.facebook.com", profile.id],
-      //   function (err, row) {
-      //     if (err) {
-      //       return cb(err);
-      //     }
-      //     if (!row) {
-      //       db.run(
-      //         "INSERT INTO users (name) VALUES (?)",
-      //         [profile.displayName],
-      //         function (err) {
-      //           if (err) {
-      //             return cb(err);
-      //           }
-      //           var id = this.lastID;
-      //           db.run(
-      //             "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
-      //             [id, "https://www.facebook.com", profile.id],
-      //             function (err) {
-      //               if (err) {
-      //                 return cb(err);
-      //               }
-      //               var user = {
-      //                 id: id,
-      //                 name: profile.displayName,
-      //               };
-      //               return cb(null, user);
-      //             }
-      //           );
-      //         }
-      //       );
-      //     } else {
-      //       db.get(
-      //         "SELECT * FROM users WHERE id = ?",
-      //         [row.user_id],
-      //         function (err, row) {
-      //           if (err) {
-      //             return cb(err);
-      //           }
-      //           if (!row) {
-      //             return cb(null, false);
-      //           }
-      //           return cb(null, row);
-      //         }
-      //       );
-      //     }
-      //   }
-      // );
     }
   )
 );
@@ -187,14 +138,14 @@ router.get(
   })
 );
 
-router.get("/login/federated/twitter", passport.authenticate("twitter"));
-router.get(
-  "/oauth/callback/twitter",
-  passport.authenticate("twitter", {
-    successReturnToOrRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
+// router.get("/login/federated/twitter", passport.authenticate("twitter"));
+// router.get(
+//   "/oauth/callback/twitter",
+//   passport.authenticate("twitter", {
+//     successReturnToOrRedirect: "/",
+//     failureRedirect: "/login",
+//   })
+// );
 
 router.get(
   "/login/federated/google",
